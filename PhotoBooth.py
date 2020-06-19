84f213aacbbf2e82af534e5b8a74199f5ee92f1e
Sleuek <arthur.petiot@etu.esisar.grenoble-inp.fr>
2020-06-19 13:21:09 +0200

    Auto commit from mac

--- PhotoBooth.py
+++ PhotoBooth.py
@@ -132,8 +132,8 @@ def watermark_with_transparency(input_image_path,
                                 position):
     base_image = Image.open(input_image_path)
     watermark = Image.open(watermark_image_path)
-    watermark.resize((LARGEUR_PHOTO,HAUTEUR_PHOTO), Image.ANTIALIAS)
-    width, height = base_image.size
+     width, height = base_image.size
+    watermark.resize((width,height), Image.ANTIALIAS)
     transparent = Image.new('RGBA', (width, height), (0,0,0,0))
     transparent.paste(base_image, (0,0))
     transparent.paste(watermark, position, mask=watermark)
