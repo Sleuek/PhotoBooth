0378b2f08dbb76a5700274d1ef392848c84bc191
Sleuek <arthur.petiot@etu.esisar.grenoble-inp.fr>
2020-06-19 11:56:25 +0200

    Auto commit from mac

--- PhotoBooth.py
+++ PhotoBooth.py
@@ -98,7 +98,10 @@ def overlayOnPreview():
     img = Image.open('/home/pi/LayerInDaSowce.png')
     # Create an image padded to the required size with
     # mode 'RGB'
-    pad = Image.new('RGB', (LARGEUR_PHOTO,HAUTEUR_PHOTO))
+    pad = Image.new('RGB', (
+        ((img.size[0] + 31) // 32) * 32,
+        ((img.size[1] + 15) // 16) * 16,
+        ))
     # Paste the original image into the padded one
     pad.paste(img, (0, 0),img)
 
