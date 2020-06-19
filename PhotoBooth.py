8f8e86b5b07e8a570216f16909d45b8a2cd65352
Sleuek <arthur.petiot@etu.esisar.grenoble-inp.fr>
2020-06-19 16:14:35 +0200

    Auto commit from mac

--- PhotoBooth.py
+++ PhotoBooth.py
@@ -19,8 +19,8 @@ from pygame.locals import *
 CurrentWorkingDir= "/var/www/html/photos/Photomaton_Prev/"
 archiveDir       = "/var/www/html/photos/PhotosduPhotomaton"
 archiveDirWithLayer = "/var/www/html/photos/PhotosduPhotomatonWithLayer"
-LARGEUR_ECRAN     = 1080
-HAUTEUR_ECRAN    = 720
+LARGEUR_ECRAN     = 1680
+HAUTEUR_ECRAN    = 1050
 LARGEUR_PHOTOPREV      = 1080 
 HAUTEUR_PHOTOPREV     = 720
 LARGEUR_PHOTO      = 1080 
